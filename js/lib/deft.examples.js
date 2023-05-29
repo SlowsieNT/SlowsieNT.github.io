6fa0800bad3c3bcc511ef067e5f565bed4d45c6d
Andrew <86121252+SlowsieNT@users.noreply.github.com>
2023-05-29 15:14:01 +0000

    Create deft.examples.js

---
+++ js/lib/deft.examples.js
@@ -0,0 +1,32 @@
+// Iterating an Array:
+deft([0, [1, 3, 4]], function (Next, aItem, aIndex, aArray, aMaxIndex) {
+	if ("undefined" !== typeof console)
+		console.log("index:", aIndex, "value:", aItem);
+	Next();
+}, function Done() {
+	if ("undefined" !== typeof console)
+		console.log("deft: done iterating.");
+	else alert("deft: done iterating.");
+});
+
+// Iterating an Array, negative start index -3:
+deft([-3, [1, 3, 4]], function (Next, aItem, aIndex, aArray, aMaxIndex) {
+	if ("undefined" !== typeof console)
+		console.log("index:", aIndex, "value:", aItem);
+	Next();
+}, function Done() {
+	if ("undefined" !== typeof console)
+		console.log("deft: done iterating.");
+	else alert("deft: done iterating.");
+});
+
+// Iterating by Number
+deft([0, 3], function (Next, aItem, aIndex, aArray, aMaxIndex) {
+	if ("undefined" !== typeof console)
+		console.log("value:", aItem);
+	Next();
+}, function Done() {
+	if ("undefined" !== typeof console)
+		console.log("deft: done iterating.");
+	else alert("deft: done iterating.");
+});
